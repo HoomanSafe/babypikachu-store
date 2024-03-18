82cae23610cd476e2b6d306f2f4de0c5602f2b06
Hooman Dev <98424162+HoomanSafe@users.noreply.github.com>
2024-03-18 17:12:04 +0800

    Add files via upload

---
+++ allowlist.tsx
@@ -0,0 +1,9 @@
+//add your allowlists here. They need to be in the same format as you used to generate your merkle root. 
+//You can also add them here and copy the route from the console log 
+export const allowLists = new Map<string, Array<string>> ([
+    
+    ["default",
+    [
+      "ed8a4ff24cb03557a8ffc7b8fe49a85d826c735ec83ab12ff80688a91628dff5",
+    ]],
+]);
---
+++ settings.tsx
@@ -0,0 +1,12 @@
+// Purpose: Contains texts that will be used in the UI when rendering the buttons
+// label has to be the exact same as in your candy machine config
+export const mintText = [
+ 
+  { label: "default", mintText: "Mint BabyPika", buttonLabel: "Mint now!", header: "$BabyPika"}
+];
+
+//header image in the ui. replace with your own
+export const image = "https://demoforhoomanclient.online/mainpage/BabyPikachu.gif";
+
+//website title
+export const headerText = "BabyPikachu ";
