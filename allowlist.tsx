89288a680ee29eaa8c5da42abf96948de1093b0e
Hooman Dev <98424162+HoomanSafe@users.noreply.github.com>
2024-03-20 07:36:31 +0800

    Update allowlist.tsx

--- allowlist.tsx
+++ allowlist.tsx
@@ -5,5 +5,6 @@ export const allowLists = new Map<string, Array<string>> ([
     ["default",
     [
       "ed8a4ff24cb03557a8ffc7b8fe49a85d826c735ec83ab12ff80688a91628dff5",
+         "36894101556fe4b4a9abeebdfc0284010a3d9957ceace83d878b408d71f1e543",
     ]],
 ]);
